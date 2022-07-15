e4542f1e101e6e71ec814f4f0d7a2af65771f709
wonyoung0207 <ahnwy27@gmail.com>
2022-07-15 18:38:27 +0900

    Uploade day66.md

--- MultiCampus/Project_TicketSaJo/SQL/Practice maaper.sql
+++ MultiCampus/Project_TicketSaJo/SQL/Practice maaper.sql
@@ -54,10 +54,14 @@ GROUP BY m.id
 ORDER BY star DESC
 LIMIT 0, 6;
 
+
+
+
 -- 오늘 상영하는 영화 
 SELECT * FROM schedules
 WHERE sdate = DATE_FORMAT(sysdate(), '%Y-%m-%d');
--- INSERT INTO schedules VALUES (null,1,1000,sysdate());
+-- INSERT INTO schedules VALUES (null,1,1002,sysdate());
+-- UPDATE schedules SET sdate=sysdate() WHERE id=2010;
 
 SELECT m.id, m.gid, m.title, m.director, m.mainactor, m.releasedate, m.posterimg1, m.posterimg2, m.country, m.runningtime, m.text,
 g.name as gname, g.tid as hgid,
@@ -71,6 +75,31 @@ WHERE s.sdate = DATE_FORMAT(sysdate(), '%Y-%m-%d')
 GROUP BY m.id;
 
 
+-- pageing 처리 방법
+select * from 
+        (select @rownum := @rownum + 1 as rn, id, title 
+        from movie, (select @rownum := 0)  as rowcolumn order by id desc) as rownum_table  
+where rn > 0 and rn <=10;
+-- where rn between 10 and 20;
+
+select * FROM(
+	SELECT title, id FROM movie order by id asc
+) m
+LIMIT 0, 3;
+
+-- review rank
+SELECT m.id, m.gid, m.title, m.director, m.mainactor, m.releasedate,
+m.posterimg1, m.posterimg2, m.country, m.runningtime, m.text,
+g.name as gname, g.tid as hgid,
+ROUND(AVG(r.star),1) as star ,COUNT(mid) as rcnt, rn
+FROM(select @rownum := @rownum + 1 as rn, id, gid, title, director, mainactor, releasedate, posterimg1,posterimg2,country,runningtime,text
+        from movie, (select @rownum := 0)  as rowcolumn order by rn desc) m
+INNER JOIN genre g ON m.gid=g.id
+INNER JOIN reviews r ON r.mid=m.id
+GROUP BY id
+ORDER BY star DESC;
+        
+        
 SELECT * FROM theater;
 SELECT * FROM schedules;
 SELECT * FROM detail_schedules;
---
+++ MultiCampus/배운 내용 List/Day66.md
@@ -0,0 +1,21 @@
+# Day66
+
+---
+
+> Final Project 
+
+# Final Project
+
+>개인 개발 시작 - 메인페이지 movie detail 페이지와 연결, google search 기능 사용 
+>
+>리뷰 rank 페이지 개발 - 페이징 처리 query
+
+## 페이징 쿼리
+
+- 출력될 페이지의 데이터만 나눠서 가져오는 것을 페이징(Paging)
+- 페이징 쿼리를 작성하는 방법에는 3가지가 있다. 
+  - Rownum 
+  - 모든 SQL에 그대로 삽입해서 사용할 수 있는 가상의 컬럼(Column) 입니다. Select를 통해 출력되는 결과 테이블에 행(row) 번호를 매겨주는 기능입니다. 
+  - limit 
+    -  LIMIT이란 select 문을 통해 데이터를 검색할 때, 검색 결과의 특정부분만 반환 받고자 할때 사용할 수 있는 기능입니다.
+  - top
